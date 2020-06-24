b0a81c742430571fc261108a2fd706f90761cbde
Zietieflr <lrmcguire93@gmail.com>
2020-06-23 21:51:54 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', alterText)
+
+function alterText() {
+  const $p = document.getElementById('text')
+  $p.textContent = 'This is really cool!'
+}
